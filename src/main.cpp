62b20092b47bff54f61f53188189bf1bff0bb0cb
muellpanda <chris@DESKTOP-66MBHRP.localdomain>
2021-01-26 11:18:55 +0100

    test code for grayscale oled display

--- src/main.cpp
+++ src/main.cpp
@@ -1,109 +1,101 @@
-#include <ILI9341_t3n.h>
+/*
+ Note:The module needs to be jumpered to an SPI interface. R19,R21 Short and R18,R20 Open  
+ Unused signal pin Recommended to connect to GND 
+  == Hardware connection ==
+    OLED   =>    Arduino
+  *1. GND    ->    GND
+  *2. VCC    ->    3.3
+  *4. SCL    ->    SCK
+  *5. SDI    ->    MOSI
+  *14. DC     ->    9
+  *15. RES    ->    8  
+  *16. CS     ->    10
+*/
+
 #include <SPI.h>
 #include <Wire.h>
-#include <XPT2046_Touchscreen.h>
-#include <lvgl.h>
 
-#include "lv_examples.h"
+#include "oled.h"
 
-#define TFT_CS 10
-#define TFT_DC 9
-ILI9341_t3n display = ILI9341_t3n(TFT_CS, TFT_DC);
+#define SCREEN_BUFFER_SIZE 8192
+#define SERIAL_BUFFER_SIZE 4
 
-#define CS_PIN 8
-#define TIRQ_PIN 2
-XPT2046_Touchscreen touch = XPT2046_Touchscreen(CS_PIN);
+static uint8_t screen_buffer[SCREEN_BUFFER_SIZE] = {};
+static char serial_buffer[SERIAL_BUFFER_SIZE] = {};
 
-#define LV_BUFFER_SIZE (320*10)
-#define LVGL_TICK_PERIOD 30
-int screenWidth = 320;
-int screenHeight = 240;
-
-int oldTouchX = 0;
-int oldTouchY = 0;
-
-/*A static or global variable to store the buffers*/
-static lv_disp_buf_t disp_buf;
-
-/*Static or global buffer(s). The second buffer is optional*/
-static lv_color_t buf_1[LV_BUFFER_SIZE];
-static lv_color_t buf_2[LV_BUFFER_SIZE];
-
-IntervalTimer tick;
-static void lv_tick_handler(void) {
-    lv_tick_inc(LVGL_TICK_PERIOD);
-}
-
-/* Display flushing */
-void drawBuffer(lv_disp_drv_t *disp, const lv_area_t *area, lv_color_t *color_p) {
-    uint16_t width = (area->x2 - area->x1 + 1);
-    uint16_t height = (area->y2 - area->y1 + 1);
-
-    display.writeRect(area->x1, area->y1, width, height, (uint16_t *)color_p);
-    lv_disp_flush_ready(disp); /* tell lvgl that flushing is done */
-}
-
-bool readInput(lv_indev_drv_t * drv, lv_indev_data_t * data) {
-    uint16_t touchX, touchY;
-
-    if (touch.touched()) {
-        TS_Point p = touch.getPixel();  // Retrieve a point
-
-        touchX = p.x;
-        touchY = p.y;
-
-        if ((touchX != oldTouchX) || (touchY != oldTouchY)) {
-            Serial.print("x= ");
-            Serial.print(touchX, DEC);
-            Serial.print(" y= ");
-            Serial.println(touchY, DEC);
-
-            oldTouchY = touchY;
-            oldTouchX = touchX;
-            data->state = LV_INDEV_STATE_PR;
-            data->point.x = touchX;
-            data->point.y = touchY;
-        }
-    } else {
-        data->point.x = oldTouchX;
-        data->point.y = oldTouchY;
-        data->state = LV_INDEV_STATE_REL;
-    }
-    return false;
-}
+void write_screen();
+void serial_communication();
 
 void setup() {
-    display.begin();
-    display.fillScreen(ILI9341_BLACK);
-    display.setRotation(3);
-
-    touch.begin(screenWidth, screenHeight, touch.getEEPROMCalibration());
-    touch.setRotation(0);
-
-    lv_init();
-    lv_disp_buf_init(&disp_buf, buf_1, buf_2, LV_BUFFER_SIZE);
-
-    /*Initialize the display*/
-    lv_disp_t *disp;
-    lv_disp_drv_t disp_drv;                 /*A variable to hold the drivers. Can be local variable*/
-    lv_disp_drv_init(&disp_drv);            /*Basic initialization*/
-    disp_drv.buffer = &disp_buf;            /*Set an initialized buffer*/
-    disp_drv.flush_cb = drawBuffer;         /*Set a flush callback to draw to the display*/
-    disp = lv_disp_drv_register(&disp_drv); /*Register the driver and save the created display objects*/
-
-    lv_indev_drv_t indev_drv;
-    lv_indev_drv_init(&indev_drv);          /*Descriptor of a input device driver*/
-    indev_drv.type = LV_INDEV_TYPE_POINTER; /*Touch pad is a pointer-like device*/
-    indev_drv.read_cb = readInput;          /*Set your driver function*/
-    lv_indev_drv_register(&indev_drv);      /*Finally register the driver*/
-
-    lv_demo_widgets();  // Start demo
-
-    Serial.println("tick.begin");
-    tick.begin(lv_tick_handler, LVGL_TICK_PERIOD * 1000);  // Start ticker
+    oled_begin();
+    oled_clear();
+    oled_bitmap_gray(PIC2);
+    delay(10);
+    
+    unsigned long ulngStart = millis();
+    Serial.begin(4608000);
+    while (!Serial && ((millis () - ulngStart) <= 666));
+    // Serial.println("Starty");
 }
 
 void loop() {
-    lv_task_handler(); /* let the GUI do its work */
-    delay(5);
+    // oled_clear();
+
+    // oled_bitmap_gray(PIC1);
+    // delay(10);
+
+    // oled_clear();
+    // oled_bitmap_gray(PIC2);
+    // delay(1000);
+
+    // oled_clear();
+    // er_oled_bitmap_mono(PIC3);
+    // delay(10);
+
+    // oled_clear();
+    // er_oled_string(0, 0, "********************************", 0);
+    // er_oled_string(32, 16, "EastRising Technology", 0);
+    // er_oled_string(40, 32, "www.buydisplay.com", 1);
+    // er_oled_string(0, 48, "********************************", 0);
+    // uint8_t i;
+    // for (i = 0; i <= 48; i++) {
+    //     command(0xa1);  //start line
+    //     data(i);
+    //     delay(100);
+    // }
+    // for (i = 48; i > 0; i--) {
+    //     command(0xa1);  //start line
+    //     data(i);
+    //     delay(100);
+    // }
+
+    serial_communication();
+    delay(10);
+}
+
+void write_screen() {
+    uint16_t bytes_written = 0;
+    Serial.println("Ok");
+    uint8_t bytes_read = Serial.readBytes(serial_buffer, 8192);
+
+    oled_bitmap_gray(screen_buffer);
+    
+    // while (bytes_written < 8192) {
+    //     //Serial.println("Ok");
+    //     uint8_t bytes_read = Serial.readBytes(serial_buffer, 8193);
+    //     bytes_written += bytes_read;
+    //     memcpy(tile + bytes_written, buf, bytes_read);
+    // }
+    // displayTextDebug(tft_2, String(bytes_written));
+}
+
+
+void serial_communication(){
+    int bytes_read;
+    if (Serial.available() > 0) {
+        bytes_read = Serial.readBytes(serial_buffer, SERIAL_BUFFER_SIZE);
+    }
+    Serial.print(serial_buffer);
+    // Serial.print("@64############################################################!@128###########################################################!@192##########################################################!@256###########################################################!");
+    // Serial.flush();
 }
